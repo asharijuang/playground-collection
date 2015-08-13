129b5ab8898a0a9714d8aa902953c7fdf60cb570
asharijuang <asharijuang@me.com>
2015-08-13 23:10:24 +0700

    basic condition

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Basic condition using if else
+var umur = 18
+
+if umur >= 17 {
+    print("Kamu sudah beranjak dewasa")
+}else {
+    print("Saat ini kamu terlalu muda")
+}
+
+var nama = "juang"
+if nama == "septi" {
+    print("hi \(nama)")
+}else {
+    print("Saya yakin namamu bukan juang")
+}
