47e8214ebc742f7ee94c4b6e36b191409c4794b8
asharijuang <asharijuang@me.com>
2015-08-13 23:20:03 +0700

    add complex condition

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -11,9 +11,38 @@ if umur >= 17 {
     print("Saat ini kamu terlalu muda")
 }
 
+// equal
 var nama = "juang"
 if nama == "septi" {
     print("hi \(nama)")
 }else {
     print("Saya yakin namamu bukan juang")
+}
+
+// 2 statement
+if nama == "juang" && umur >= 17 {
+    print("Hi \(nama), kamu sudah beranjak dewasa")
+}else {
+    print("Maaf saya sedang mencari juang")
+}
+
+// statemen or menggunakan tanda ||
+
+// statement with boolean
+var keputusan = true
+
+if keputusan {
+    print("Ini yang terbaik buat kamu")
+}
+
+// else if
+var username = "admin"
+var pass = "password"
+
+if username == "admin" && pass == "" {
+    print("Password anda salah atau password tidak boleh kosong")
+}else if username == "" && pass == "password" {
+    print("Username tidak boleh kosong")
+}else {
+    print("Selamat datang")
 }
